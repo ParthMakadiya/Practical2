b434718a81cd5f37a634022ed8d7c2f3c19f082f
ParthMakadiya <46336477+ParthMakadiya@users.noreply.github.com>
2019-02-13 22:43:23 +0530

    Add files via upload

---
+++ Pattern1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pattern1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                for (int j = 0; j <i; j++)
+                {
+                    Console.Write("@");
+                }
+                Console.WriteLine();
+            }
+    	Console.ReadKey();
+        }
+    }
+}
